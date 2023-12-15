3f307a2d6d11f9097603a199a00dfb18302f1cd2
MartinAlbanesi <martinalbanesi89@gmail.com>
2023-12-15 18:15:32 -0300

    feat: tests for Homework class

---
+++ UnitTesting/app/src/main/java/com/example/unittesting/Homework.kt
@@ -0,0 +1,34 @@
+package com.example.unittesting
+
+object Homework {
+
+    /**
+     * Returns the n-th Fibonacci number.
+     * they are defined like this:
+     * fib(0) = 0
+     * fib(1) = 1
+     * fib(n) = fib(n-2) + fib(n-1)
+     */
+    fun fib(n: Int): Long {
+        if (n == 0 || n == 1) {
+            return n.toLong()
+        }
+        var a = 0L
+        var b = 1L
+        var c = 1L
+        (1..<n).forEach { i ->
+            c = a + b
+            a = b
+            b = c
+        }
+        return c
+    }
+
+    /**
+     * Checks if the braces are set correctly
+     * e.g. "(a * b))" should return false
+     */
+    fun checkBraces(string: String): Boolean {
+        return string.count { it == '(' } == string.count { it == ')' }
+    }
+}
